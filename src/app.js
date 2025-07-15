96bbd8a29bdde8ceaf11b761168bee8e8d626a80
sagar7378 <sagarprajapat250@gmail.com>
2025-07-15 20:31:58 +0530

    cors allow

--- src/app.js
+++ src/app.js
@@ -25,7 +25,8 @@ app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: false }));
 app.use(cors(
-  "http://192.168.56.1:5173/"
+  "http://192.168.56.1:5173/",
+  "https://dev-chat-iota.vercel.app/"
 ));
 
 
